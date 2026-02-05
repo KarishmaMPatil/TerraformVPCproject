77c78d5c333ba9e1c8f729e7ca83a4b24b5ffd9d
KarishmaMPatil <karipatil16@gmail.com>
2026-02-05 17:11:23 +0000

    new changes

---
+++ terraformb32/module/module/main.tf
@@ -0,0 +1,19 @@
+module "ec2" {
+  source = "/home/karipatil/TerraformVPCproject/terraformb32/module/main/ec2"
+  webserver_ami = "ami-019715e0d74f695be"      
+  webserver_keyname = "ubuntu"
+  webserver_disable_api_termination = false
+  webserver_sg = module.vpc.webserversg   #attache security group to ec2 module name vpc webserversg from output block
+  webserver_subnetA = module.vpc.subnet_id  #subnet id from output block 
+
+
+} 
+
+module "vpc" {
+  source =  "/home/karipatil/TerraformVPCproject/terraformb32/module/main/vpc"
+  vpc_cidr = "172.30.0.0/16"   #vpc variable block unique name
+  vpc_subnetA = "172.30.128.0/20"
+  public_ip = true
+  az = "us-east-1a"
+
+}
